48520ebbcf7b6ee7b5285005280cf85c57e70c18
Tạ Quang Khôi <khoi0941@gmail.com>
2023-07-27 14:10:58 +0700

    Put Column to a Stack

--- lib/pages/create_account/introduction_page.dart
+++ lib/pages/create_account/introduction_page.dart
@@ -8,43 +8,47 @@ class NgNhP extends StatelessWidget {
     return const Scaffold(
       backgroundColor: Color(0xFFF1F9EA),
       appBar: null,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+      body: Stack(
         children: [
-
-          Image(image: AssetImage('assets/introduction_page/hinh3.png')),
-          Text(
-            'TIẾP TỤC',
-            textAlign: TextAlign.center,
-            // style: TextStyle(
-            //   color: Colors.white,
-            //   fontSize: 18,
-            //   fontFamily: 'Mulish',
-            //   fontWeight: FontWeight.w400,
-            //   letterSpacing: -0.30,
-            // ),
-          ),
-          Text(
-            'Nơi bạn yêu những thứ cũ kỹ ',
-            textAlign: TextAlign.center,
-            // style: TextStyle(
-            //   color: Color(0xFF293731),
-            //   fontSize: 24,
-            //   fontFamily: 'Mulish',
-            //   fontWeight: FontWeight.w700,
-            //   height: 36,
-            // ),
-          ),
-          Text(
-            'Bạn tìm nơi cất giữ những điều cũ kỹ\nEX.BOOK tìm nơi cất giữ những \nnhững quyển sách cũ kỹ ',
-            textAlign: TextAlign.center,
-            // style: TextStyle(
-            //   color: Color(0xFF293731),
-            //   fontSize: 16,
-            //   fontFamily: 'Mulish',
-            //   fontWeight: FontWeight.w700,
-            //   height: 24,
-            // ),
+          
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image(image: AssetImage('assets/introduction_page/hinh3.png')),
+              Text(
+                'TIẾP TỤC',
+                textAlign: TextAlign.center,
+                // style: TextStyle(
+                //   color: Colors.white,
+                //   fontSize: 18,
+                //   fontFamily: 'Mulish',
+                //   fontWeight: FontWeight.w400,
+                //   letterSpacing: -0.30,
+                // ),
+              ),
+              Text(
+                'Nơi bạn yêu những thứ cũ kỹ ',
+                textAlign: TextAlign.center,
+                // style: TextStyle(
+                //   color: Color(0xFF293731),
+                //   fontSize: 24,
+                //   fontFamily: 'Mulish',
+                //   fontWeight: FontWeight.w700,
+                //   height: 36,
+                // ),
+              ),
+              Text(
+                'Bạn tìm nơi cất giữ những điều cũ kỹ\nEX.BOOK tìm nơi cất giữ những \nnhững quyển sách cũ kỹ ',
+                textAlign: TextAlign.center,
+                // style: TextStyle(
+                //   color: Color(0xFF293731),
+                //   fontSize: 16,
+                //   fontFamily: 'Mulish',
+                //   fontWeight: FontWeight.w700,
+                //   height: 24,
+                // ),
+              )
+            ],
           )
         ],
       ),
