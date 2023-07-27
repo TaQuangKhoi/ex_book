33ea15bcb9465611249a3525ec026456f8218fae
Tạ Quang Khôi <khoi0941@gmail.com>
2023-07-27 15:23:33 +0700

    Create enum ExBookColor

---
+++ lib/library/ex_book_colors.dart
@@ -0,0 +1,12 @@
+import 'dart:ui';
+
+enum ExBookColor {
+  mauChinh1(0xFF293731), mauChinh2(0xFF293731), mauChinh3(0xFFF1F9EA),
+
+  mauPhu1(0xFF293731), mauPhu2(0xFF293731),
+
+  mauTrungTinh1(0xFF293731), mauTrungTinh2(0xFF293731), mauTrungTinh3(0xFF293731), mauTrungTinh4(0xFF293731), mauTrungTinh5(0xFF293731);
+
+  final int colorHex;
+  const ExBookColor(this.colorHex);
+}
