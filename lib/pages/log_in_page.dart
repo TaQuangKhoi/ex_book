280555a11ab4426d05eeb3c905259226682ada60
Tạ Quang Khôi <khoi0941@gmail.com>
2023-08-02 21:49:07 +0700

    Create LogInPage and make route for it

---
+++ lib/pages/log_in_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LogInPage extends StatefulWidget {
+  const LogInPage({super.key});
+
+  @override
+  State<LogInPage> createState() => _LogInPageState();
+}
+
+class _LogInPageState extends State<LogInPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
