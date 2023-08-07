e4506fac3750e46907271f878e8c513c794e578a
Tạ Quang Khôi <khoi0941@gmail.com>
2023-08-07 14:22:40 +0700

    Sử dụng Padding 18.0

--- lib/pages/sign_up_page.dart
+++ lib/pages/sign_up_page.dart
@@ -45,139 +45,140 @@ class _SignUpPageState extends State<SignUpPage> {
       backgroundColor: Color(ExBookColor.mauChinh3.colorHex),
       resizeToAvoidBottomInset: false,
       body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(
-              'ĐĂNG KÝ TÀI KHOẢN',
-              style: GoogleFonts.mulish(
-                textStyle: const TextStyle(
+        child: Padding(
+          padding: EdgeInsets.all(18.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                'ĐĂNG KÝ TÀI KHOẢN',
+                style: GoogleFonts.mulish(
+                    textStyle: const TextStyle(
                   color: Color(0xFF2A3732),
                   fontSize: 16,
                   fontWeight: FontWeight.w700,
                   letterSpacing: -0.30,
-                )
+                )),
               ),
-            ),
-            Text(
-              'Bạn hãy nhập thông tin đã đăng ký tài khoản',
-              style: GoogleFonts.mulish(
-                textStyle: const TextStyle(
+              Text(
+                'Bạn hãy nhập thông tin đã đăng ký tài khoản',
+                style: GoogleFonts.mulish(
+                  textStyle: const TextStyle(
+                    color: Color(0xFF2A3732),
+                    fontSize: 16,
+                    fontWeight: FontWeight.w400,
+                    // letterSpacing: -0.30,
+                  ),
+                ),
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Tên tài khoản',
+                    style: labelTextFieldStyle,
+                  ),
+                  TextField(
+                    decoration: getTextFieldStyle('Name_123'),
+                  ),
+                ],
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Email / Điện thoại',
+                    style: labelTextFieldStyle,
+                  ),
+                  TextField(
+                    decoration: getTextFieldStyle('@gmail.com'),
+                  ),
+                ],
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Ngày sinh',
+                    style: labelTextFieldStyle,
+                  ),
+                  TextField(
+                    decoration: getTextFieldStyle('DD/MM/YY'),
+                  ),
+                ],
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Giới tính',
+                    style: labelTextFieldStyle,
+                  ),
+                  TextField(
+                    decoration: getTextFieldStyle('Nam/Nữ'),
+                  ),
+                ],
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Mật khẩu',
+                    style: labelTextFieldStyle,
+                  ),
+                  TextField(
+                    decoration: getTextFieldStyle('*********'),
+                  ),
+                ],
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Nhập lại mật khẩu',
+                    style: labelTextFieldStyle,
+                  ),
+                  TextField(
+                    decoration: getTextFieldStyle('*********'),
+                  ),
+                ],
+              ),
+              const Center(
+                child: ElevatedButton(
+                    onPressed: null,
+                    child: Text(
+                      'ĐĂNG NHẬP',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                        fontFamily: 'Mulish',
+                        fontWeight: FontWeight.w400,
+                        letterSpacing: -0.30,
+                      ),
+                    )),
+              ),
+              const Text(
+                'Hoặc',
+                textAlign: TextAlign.center,
+                style: TextStyle(
                   color: Color(0xFF2A3732),
                   fontSize: 16,
                   fontFamily: 'Mulish',
-                  fontWeight: FontWeight.w400,
-                  // letterSpacing: -0.30,
+                  fontWeight: FontWeight.w500,
+                  letterSpacing: -0.30,
                 ),
               ),
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Tên tài khoản',
-                  style: labelTextFieldStyle,
-                ),
-                TextField(
-                  decoration: getTextFieldStyle('Name_123'),
-                ),
-              ],
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Email / Điện thoại',
-                  style: labelTextFieldStyle,
-                ),
-                TextField(
-                  decoration: getTextFieldStyle('@gmail.com'),
-                ),
-              ],
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Ngày sinh',
-                  style: labelTextFieldStyle,
-                ),
-                TextField(
-                  decoration: getTextFieldStyle('DD/MM/YY'),
-                ),
-              ],
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Giới tính',
-                  style: labelTextFieldStyle,
-                ),
-                TextField(
-                  decoration: getTextFieldStyle('Nam/Nữ'),
-                ),
-              ],
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Mật khẩu',
-                  style: labelTextFieldStyle,
-                ),
-                TextField(
-                  decoration: getTextFieldStyle('*********'),
-                ),
-              ],
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Nhập lại mật khẩu',
-                  style: labelTextFieldStyle,
-                ),
-                TextField(
-                  decoration: getTextFieldStyle('*********'),
-                ),
-              ],
-            ),
-            const Center(
-              child: ElevatedButton(
-                  onPressed: null,
-                  child: Text(
-                    'ĐĂNG NHẬP',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 18,
-                      fontFamily: 'Mulish',
-                      fontWeight: FontWeight.w400,
-                      letterSpacing: -0.30,
-                    ),
-                  )),
-            ),
-            const Text(
-              'Hoặc',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Color(0xFF2A3732),
-                fontSize: 16,
-                fontFamily: 'Mulish',
-                fontWeight: FontWeight.w500,
-                letterSpacing: -0.30,
-              ),
-            ),
-            ElevatedButton(onPressed: null, child: fbLogoSvg),
-          ],
+              ElevatedButton(onPressed: null, child: fbLogoSvg),
+            ],
+          ),
         ),
       ),
     );
