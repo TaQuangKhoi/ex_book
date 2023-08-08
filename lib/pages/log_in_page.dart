6944b4748d0f70b7f7c197ed7b38341b0aaaa4e0
Tạ Quang Khôi <khoi0941@gmail.com>
2023-08-08 21:42:03 +0700

    Demo Login Page with Đăng Nhập

--- lib/pages/log_in_page.dart
+++ lib/pages/log_in_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import '../library/ex_book_colors.dart';
+import '../widgets/facebook_google_sign_in.dart';
 
 class LogInPage extends StatefulWidget {
   const LogInPage({super.key});
@@ -10,6 +14,62 @@ class LogInPage extends StatefulWidget {
 class _LogInPageState extends State<LogInPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: null,
+      backgroundColor: Color(ExBookColor.background.colorHex),
+      resizeToAvoidBottomInset: false,
+      body: SafeArea(
+        child: Padding(
+    padding: const EdgeInsets.all(18.0),
+        child: Stack(
+          children: [
+            Positioned(
+                bottom: 0,
+                left: 0,
+                right: 0,
+                child: Column(
+                  children: [
+                    SizedBox(
+                      width: double.infinity,
+                      height: 56,
+                      child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor:
+                            Color(ExBookColor.mauChinh1.colorHex),
+                          ),
+                          onPressed: () {},
+                          child: Text(
+                            'ĐĂNG NHẬP',
+                            textAlign: TextAlign.center,
+                            style: GoogleFonts.mulish(
+                              textStyle: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 18,
+                                fontWeight: FontWeight.w400,
+                                letterSpacing: -0.30,
+                              ),
+                            ),
+                          )),
+                    ),
+                    Text(
+                      'Hoặc',
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.mulish(
+                        textStyle: TextStyle(
+                          color:
+                          Color(ExBookColor.mauTrungTinh1.colorHex),
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                          letterSpacing: -0.30,
+                        ),
+                      ),
+                    ),
+                    const FacebookGoogleSignIn()
+                  ],
+                )),
+          ],
+        )),
+      ),
+    );
   }
 }
