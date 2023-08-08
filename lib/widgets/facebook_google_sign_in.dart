eac4b90d8a2086e5fee36174643f3f25b24e74e3
Tạ Quang Khôi <khoi0941@gmail.com>
2023-08-08 20:31:04 +0700

    Create widget FacebookGoogleSignIn

---
+++ lib/widgets/facebook_google_sign_in.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FacebookGoogleSignIn extends StatefulWidget {
+  const FacebookGoogleSignIn({super.key});
+
+  @override
+  State<FacebookGoogleSignIn> createState() => _FacebookGoogleSignInState();
+}
+
+class _FacebookGoogleSignInState extends State<FacebookGoogleSignIn> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
