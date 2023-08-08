fe26a88e3b523ac2993ebbe04b73fe1da0a2b8f0
Tạ Quang Khôi <khoi0941@gmail.com>
2023-08-08 18:18:23 +0700

    Create ExBookTextField

---
+++ lib/widgets/ExBookTextField.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ExBookTextField extends StatefulWidget {
+  const ExBookTextField({super.key});
+
+  @override
+  State<ExBookTextField> createState() => _ExBookTextFieldState();
+}
+
+class _ExBookTextFieldState extends State<ExBookTextField> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
