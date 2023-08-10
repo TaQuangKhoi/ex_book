9310ec585139317d99a51bbf2ae7a247dee4175c
Tạ Quang Khôi <khoi0941@gmail.com>
2023-08-10 20:43:42 +0700

    Done Bottom Navigation Bar

--- lib/pages/navigation_page.dart
+++ lib/pages/navigation_page.dart
@@ -1,4 +1,6 @@
+import 'package:ex_book/library/ex_book_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class NavigationPage extends StatefulWidget {
   const NavigationPage({super.key});
@@ -12,27 +14,49 @@ class _NavigationPageState extends State<NavigationPage> {
 
   @override
   Widget build(BuildContext context) {
+    const String assetPath = 'assets/navigation_page/';
+
     return Scaffold(
       bottomNavigationBar: NavigationBar(
+        indicatorColor: Colors.transparent,
+        backgroundColor: Color(ExBookColor.mauChinh3.colorHex),
         onDestinationSelected: (int index) {
           setState(() {
             currentPageIndex = index;
           });
         },
         selectedIndex: currentPageIndex,
-        destinations: const <Widget>[
+        destinations: <Widget>[
           NavigationDestination(
-            icon: Icon(Icons.explore),
-            label: 'Explore',
+            selectedIcon: SvgPicture.asset('${assetPath}selected-home-icon.svg',
+                semanticsLabel: 'Selected Home Icon'),
+            icon: SvgPicture.asset('${assetPath}home-icon.svg',
+                semanticsLabel: 'Home Icon'),
+            label: 'Trang chủ',
           ),
           NavigationDestination(
-            icon: Icon(Icons.commute),
-            label: 'Commute',
+            selectedIcon: SvgPicture.asset(
+                '${assetPath}selected-book-plus-icon.svg',
+                semanticsLabel: 'Selected Buy Book Icon'),
+            icon: SvgPicture.asset('${assetPath}book-plus-icon.svg',
+                semanticsLabel: 'Buy Book Icon'),
+            label: 'Mua sách',
           ),
           NavigationDestination(
-            selectedIcon: Icon(Icons.bookmark),
-            icon: Icon(Icons.bookmark_border),
-            label: 'Saved',
+            selectedIcon: SvgPicture.asset(
+                '${assetPath}selected-book-sync-icon.svg',
+                semanticsLabel: 'Selected Book Sync Icon'),
+            icon: SvgPicture.asset('${assetPath}book-sync-icon.svg',
+                semanticsLabel: 'Book Sync Icon'),
+            label: 'Đổi sách',
+          ),
+          NavigationDestination(
+            selectedIcon: SvgPicture.asset(
+                '${assetPath}selected-address-book-icon.svg',
+                semanticsLabel: 'Selected Address Book Icon'),
+            icon: SvgPicture.asset('${assetPath}address-book-icon.svg',
+                semanticsLabel: 'Address Book Icon'),
+            label: 'Cá nhân',
           ),
         ],
       ),
@@ -52,9 +76,12 @@ class _NavigationPageState extends State<NavigationPage> {
           alignment: Alignment.center,
           child: const Text('Page 3'),
         ),
+        Container(
+          color: Colors.indigo,
+          alignment: Alignment.center,
+          child: const Text('Page 4'),
+        ),
       ][currentPageIndex],
     );
   }
 }
-
-
