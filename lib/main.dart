17269c8e8893c0829f39ef78e4cf24dd942a6268
Tạ Quang Khôi <khoi0941@gmail.com>
2023-08-17 16:15:56 +0700

    init Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
-import 'package:flutter/services.dart';
-
 import 'package:flutter/material.dart';
 
 import 'ex_book_app.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const ExBookApp());
 }
