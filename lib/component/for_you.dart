e2a69a21d71495ff842bfd19fce513fbc314cbc8
Tạ Quang Khôi <khoi0941@gmail.com>
2023-08-18 01:07:52 +0700

    add component ForYou

---
+++ lib/component/for_you.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import '../model/book.dart';
+import '../widgets/book_box.dart';
+
+class ForYou extends StatefulWidget {
+  const ForYou({super.key});
+
+  @override
+  State<ForYou> createState() => _ForYouState();
+}
+
+class _ForYouState extends State<ForYou> {
+  List books = [
+    Book(
+        name: 'Sherlock Holmes',
+        author: 'Conan Doyle',
+        description:
+        'Sherlock Holmes là một nhân vật thám tử hư cấu vào cuối thể kỉ 19 và đầu thế kỉ 20,....',
+        imagePath: 'assets/home_page/sherlock_homes.png'),
+    Book(
+        author: 'J. K. Rowling.',
+        name: 'Harry Potter',
+        description:
+        'Cậu bé Harry Potter một mình chống lại một phù thủy hắc ám Chúa tể Voldemort,...',
+        imagePath: 'assets/home_page/sherlock_homes.png'),
+    Book(
+        author: 'J. K. Rowling.',
+        name: 'Harry Potter',
+        description:
+        'Cậu bé Harry Potter một mình chống lại một phù thủy hắc ám Chúa tể Voldemort,...',
+        imagePath: 'assets/home_page/sherlock_homes.png'),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 8),
+      child: Column(
+        children: [
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Padding(
+              padding: const EdgeInsets.only(left: 16),
+              child: Text(
+                'Dành cho bạn',
+                style: GoogleFonts.mulish(
+                  textStyle: const TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                    fontWeight: FontWeight.w700,
+                    letterSpacing: -0.30,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          ListView.builder(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            itemBuilder: (context, index) => Padding(
+                padding: const EdgeInsets.only(top: 8),
+                child: BookBox(
+                  bookName: books[index].name,
+                  authorName: books[index].author,
+                  description: books[index].description,
+                  imagePath: books[index].imagePath,
+                )),
+            itemCount: books.length,
+            physics: const NeverScrollableScrollPhysics(),
+            primary: false,
+            shrinkWrap: true,
+          ),
+        ],
+      ),
+    );
+  }
+}
