764f011ebdf88a9c5404f89afd4a5c971e98388d
Tạ Quang Khôi <khoi0941@gmail.com>
2023-08-25 00:29:48 +0700

    Make Text Lưu và Quên mật  khẩu

--- lib/pages/love_at_first_sight/log_in_page.dart
+++ lib/pages/love_at_first_sight/log_in_page.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -17,6 +19,7 @@ class _LogInPageState extends State<LogInPage> {
   TextEditingController usernameController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
   final _formKey = GlobalKey<FormState>();
+  bool IsSavePassword = false;
 
   @override
   Widget build(BuildContext context) {
@@ -26,101 +29,139 @@ class _LogInPageState extends State<LogInPage> {
       resizeToAvoidBottomInset: false,
       body: SafeArea(
         child: Padding(
-    padding: const EdgeInsets.all(18.0),
-        child: Stack(
-          children: [
-            Form(
-              key: _formKey,
-              child: Column(
-                children: [
-                  ExBookTextField(
-                    text: 'Email/ Điện thoại',
-                    textFieldText: '@gmail.com',
-                    controller: usernameController,
-                    isPassword: false,
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter some text';
-                      }
-                      return null;
-                    },
-                  ),
-                  ExBookTextField(
-                    text: 'Mật khẩu',
-                    textFieldText: '*********',
-                    controller: passwordController,
-                    isPassword: true,
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter some text';
-                      }
-                      return null;
-                    },
-                  ),
-                  const Row(
+            padding: const EdgeInsets.all(18.0),
+            child: Stack(
+              children: [
+                Form(
+                  key: _formKey,
+                  child: Column(
                     children: [
-                      Text('Quên mật khẩu?'),
-                    ],
-                  )
-                ],
-              ),
-            ),
-            Positioned(
-                bottom: 0,
-                left: 0,
-                right: 0,
-                child: Column(
-                  children: [
-                    SizedBox(
-                      width: double.infinity,
-                      height: 56,
-                      child: ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor:
-                            Color(ExBookColor.mauChinh1.colorHex),
-                          ),
-                          onPressed: () {
-                            if (_formKey.currentState!.validate()) {
-                              // If the form is valid, display a snackbar. In the real world,
-                              // you'd often call a server or save the information in a database.
-                              // ScaffoldMessenger.of(context).showSnackBar(
-                              //   const SnackBar(content: Text('Processing Data')),
-                              // );
-                              Navigator.push(context,
-                                  MaterialPageRoute(builder: (context) => const NavigationPage()));
-                            }
-                          },
-                          child: Text(
-                            'ĐĂNG NHẬP',
-                            textAlign: TextAlign.center,
-                            style: GoogleFonts.mulish(
-                              textStyle: const TextStyle(
-                                color: Colors.white,
-                                fontSize: 18,
-                                fontWeight: FontWeight.w400,
-                                letterSpacing: -0.30,
-                              ),
-                            ),
-                          )),
-                    ),
-                    Text(
-                      'Hoặc',
-                      textAlign: TextAlign.center,
-                      style: GoogleFonts.mulish(
-                        textStyle: TextStyle(
-                          color:
-                          Color(ExBookColor.mauTrungTinh1.colorHex),
-                          fontSize: 16,
-                          fontWeight: FontWeight.w500,
-                          letterSpacing: -0.30,
-                        ),
+                      ExBookTextField(
+                        text: 'Email/ Điện thoại',
+                        textFieldText: '@gmail.com',
+                        controller: usernameController,
+                        isPassword: false,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter some text';
+                          }
+                          return null;
+                        },
                       ),
-                    ),
-                    const FacebookGoogleSignIn()
-                  ],
-                )),
-          ],
-        )),
+                      ExBookTextField(
+                        text: 'Mật khẩu',
+                        textFieldText: '*********',
+                        controller: passwordController,
+                        isPassword: true,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter some text';
+                          }
+                          return null;
+                        },
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            children: [
+                              Checkbox(
+                                value: IsSavePassword,
+                                onChanged: (bool? value) {
+                                  setState(() {
+                                    IsSavePassword = value!;
+                                  });
+                                },
+                              ),
+                              Text(
+                                'Lưu mật khẩu!',
+                                style: GoogleFonts.mulish(
+                                    textStyle: const TextStyle(
+                                  color: Color.fromRGBO(42, 55, 50, 0.50),
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.w400,
+                                  letterSpacing: -0.30,
+                                )),
+                              ),
+                            ],
+                          ),
+                          TextButton(
+                              onPressed: () {
+                                log('Quên mật khẩu?');
+                              },
+                              child: Text(
+                                'Quên mật khẩu?',
+                                style: GoogleFonts.mulish(
+                                    textStyle: const TextStyle(
+                                  color: Color.fromRGBO(42, 55, 50, 0.50),
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.w400,
+                                  letterSpacing: -0.30,
+                                )),
+                              )),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+                Positioned(
+                    bottom: 0,
+                    left: 0,
+                    right: 0,
+                    child: Column(
+                      children: [
+                        SizedBox(
+                          width: double.infinity,
+                          height: 56,
+                          child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor:
+                                    Color(ExBookColor.mauChinh1.colorHex),
+                              ),
+                              onPressed: () {
+                                if (_formKey.currentState!.validate()) {
+                                  // If the form is valid, display a snackbar. In the real world,
+                                  // you'd often call a server or save the information in a database.
+                                  // ScaffoldMessenger.of(context).showSnackBar(
+                                  //   const SnackBar(content: Text('Processing Data')),
+                                  // );
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) =>
+                                              const NavigationPage()));
+                                }
+                              },
+                              child: Text(
+                                'ĐĂNG NHẬP',
+                                textAlign: TextAlign.center,
+                                style: GoogleFonts.mulish(
+                                  textStyle: const TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.w400,
+                                    letterSpacing: -0.30,
+                                  ),
+                                ),
+                              )),
+                        ),
+                        Text(
+                          'Hoặc',
+                          textAlign: TextAlign.center,
+                          style: GoogleFonts.mulish(
+                            textStyle: TextStyle(
+                              color: Color(ExBookColor.mauTrungTinh1.colorHex),
+                              fontSize: 16,
+                              fontWeight: FontWeight.w500,
+                              letterSpacing: -0.30,
+                            ),
+                          ),
+                        ),
+                        const FacebookGoogleSignIn()
+                      ],
+                    )),
+              ],
+            )),
       ),
     );
   }
