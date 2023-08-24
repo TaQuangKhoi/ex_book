ebe5b773fa66c4e4ab962bf455c2bcb8f457bcc8
Tạ Quang Khôi <khoi0941@gmail.com>
2023-08-24 23:23:18 +0700

    Tách IntroBackground3

--- lib/pages/love_at_first_sight/introduction_page/page.dart
+++ lib/pages/love_at_first_sight/introduction_page/page.dart
@@ -7,6 +7,7 @@ import '../../../library/ex_book_colors.dart';
 import '../../../widgets/text_between.dart';
 import 'background_1.dart';
 import 'background_2.dart';
+import 'background_3.dart';
 import 'button_login_and_sign_up.dart';
 
 class IntroductionPage extends StatefulWidget {
@@ -147,120 +148,6 @@ class _IntroductionPageState extends State<IntroductionPage> {
           ],
         ),
       ),
-    );
-  }
-}
-
-
-
-class IntroBackground3 extends StatelessWidget {
-  const IntroBackground3({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Stack(
-      alignment: Alignment.center,
-      children: [
-        const Positioned(
-          top: 100,
-          left: 0,
-          child: Image(
-            image:
-                AssetImage('assets/introduction_page/background3/vector_5.png'),
-          ),
-        ),
-        const Positioned(
-          top: 120,
-          left: 0,
-          child: Image(
-            image:
-                AssetImage('assets/introduction_page/background3/vector_3.png'),
-          ),
-        ),
-        const Positioned(
-          top: 120,
-          left: 20,
-          child: Image(
-            image: AssetImage(
-                'assets/introduction_page/background3/group_105.png'),
-          ),
-        ),
-        const Positioned(
-          top: 160,
-          left: 0,
-          child: Image(
-            image: AssetImage(
-                'assets/introduction_page/background3/group_106.png'),
-          ),
-        ),
-        const Positioned(
-          top: 0,
-          right: 0,
-          child: Image(
-            image:
-                AssetImage('assets/introduction_page/background3/vector_1.png'),
-          ),
-        ),
-        const Positioned(
-          top: 0,
-          right: 0,
-          child: Image(
-            image:
-                AssetImage('assets/introduction_page/background3/vector_6.png'),
-          ),
-        ),
-        const Positioned(
-          top: 20,
-          right: 120,
-          child: Image(
-            image: AssetImage(
-                'assets/introduction_page/background3/group_102.png'),
-          ),
-        ),
-        const Positioned(
-          top: 40,
-          right: 60,
-          child: Image(
-            image: AssetImage(
-                'assets/introduction_page/background3/group_102.png'),
-          ),
-        ),
-        const Positioned(
-          top: 40,
-          right: 0,
-          child: Image(
-            image: AssetImage(
-                'assets/introduction_page/background3/group_104.png'),
-          ),
-        ),
-        Positioned(
-          bottom: 0,
-          right: 0,
-          child: SizedBox(
-            width: MediaQuery.of(context).size.width,
-            child: const Image(
-              fit: BoxFit.fill,
-              image: AssetImage(
-                  'assets/introduction_page/background3/vector_4.png'),
-            ),
-          ),
-        ),
-        const Positioned(
-          bottom: 0,
-          right: 0,
-          child: Image(
-            image:
-                AssetImage('assets/introduction_page/background3/vector_2.png'),
-          ),
-        ),
-        const TextBetween(
-          circleIndex: 3,
-          text1: 'Những cuốn sách tái sinh',
-          text2:
-              'Chúng mình là EX.BOOK nơi tái sinh \nnhững quyển sách cũ kỹ trên tay bạn',
-          imagePath: 'assets/introduction_page/background3/hinh_sach_1_1.png',
-        ),
-      ],
     );
   }
 }
